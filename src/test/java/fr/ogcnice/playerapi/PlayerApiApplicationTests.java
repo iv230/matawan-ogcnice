4d72f767fd81d8a7ab6f8267683e9c2c738381b6
iv230 <lucas.kessayan@gmail.com>
2023-07-20 11:14:22 +0200

    Inital commit

---
+++ src/test/java/fr/ogcnice/playerapi/PlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ogcnice.playerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
